0232991fabcb597a5382993d6ce589edd64df99a
asarenski <asarenski@gmail.com>
2018-02-11 16:57:01 -0500

    removed fills for specific reason

--- src/index.js
+++ src/index.js
@@ -2,22 +2,18 @@ require('babel-polyfill');
 import { parseAsync } from './csvParser';
 import _ from 'lodash';
 
+// Note: only use the history from GDAX. The buys and sells throw things off as that misses certain 'match' entries.
+// still need to figure out what those match entries are representing.
+
 const historyFilePath = '/home/asarenski/Downloads/history.csv';
 (async function() {
     const historyJson = await parseAsync(historyFilePath);
-    const result = _.chain(historyJson)
-        .filter(({ type }) => type !== 'match')
+    const historySum = _.chain(historyJson)
         .reduce((sum, { type, amount }) => {
             const floatAmount = parseFloat(amount);
-            switch (type) {
-                case 'withdrawal':
-                    return sum -= floatAmount;
-                case 'deposit':
-                    return sum += floatAmount;
-                default:
-                    return sum;
-            }
+            return floatAmount ? sum += floatAmount : sum;
         }, 0)
         .value();
-    console.log("sum is : ", result);
+
+    console.log('history sum: ', historySum);
 })();
