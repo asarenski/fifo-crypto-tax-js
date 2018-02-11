0d18d3c35feece18a75e6e133a5cfa0901a298c5
asarenski <asarenski@gmail.com>
2018-02-11 16:36:17 -0500

    doing some lodash stuff

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 require('babel-polyfill');
 import { parseAsync } from './csvParser';
+import _ from 'lodash';
 
-const filePath = '/home/asarenski/Downloads/history.csv';
+const historyFilePath = '/home/asarenski/Downloads/history.csv';
 (async function() {
-    const csvAsJSON = await parseAsync(filePath);
-    console.log(csvAsJSON);
-    console.log('end...');
+    const historyJson = await parseAsync(historyFilePath);
+    const result = _.chain(historyJson)
+        .filter(({ type }) => type !== 'match')
+        .reduce((sum, { type, amount }) => {
+            const floatAmount = parseFloat(amount);
+            switch (type) {
+                case 'withdrawal':
+                    return sum -= floatAmount;
+                case 'deposit':
+                    return sum += floatAmount;
+                default:
+                    return sum;
+            }
+        }, 0)
+        .value();
+    console.log("sum is : ", result);
 })();
