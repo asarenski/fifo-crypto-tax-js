a4b88cca08a381a335c22b8d45af3d557583281c
asarenski <asarenski@gmail.com>
2018-02-12 18:38:36 -0500

    removed console log

--- src/fifoRecursive/createFifoEntry.js
+++ src/fifoRecursive/createFifoEntry.js
@@ -1,6 +1,5 @@
 const createFifoEntry = (buy, sell) => {
   const amount = buy.amount - sell.amount >= 0 ? sell.amount : buy.amount;
-  console.log(amount)
   return {
     amount,
     buyDate: buy.time,
