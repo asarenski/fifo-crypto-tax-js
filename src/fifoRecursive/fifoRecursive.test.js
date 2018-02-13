d5922887c79006b13d928aa65d591646a52941cd
asarenski <asarenski@gmail.com>
2018-02-12 19:34:13 -0500

    one more test

--- src/fifoRecursive/fifoRecursive.test.js
+++ src/fifoRecursive/fifoRecursive.test.js
@@ -1,6 +1,7 @@
 import fifoRecursive from './fifoRecursive';
 import Queue from '../Queue';
 import moment from 'moment';
+import { fixFloat } from '../mathUtil';
 
 const mockTransaction = (amount, time) => ({amount, time});
 const generateSellEntry = (amount, buyDate, saleDate) => ({amount, buyDate, saleDate});
@@ -32,6 +33,22 @@ describe('fifoRecursive', () => {
     expect(sellEntries).toEqual([
       generateSellEntry(buy1.amount, buy1.time, sell.time),
       generateSellEntry(buy2.amount, buy2.time, sell.time),
+    ]);
+  });
+
+  it('restacks the last buy amount minus the sale amount if buy amount greater than sale amount', () => {
+    const buy = mockTransaction(0.777, moment().subtract(10, 'minutes'));
+    const buys = new Queue([buy]);
+
+    const sell = mockTransaction(0.333, moment());
+    const { buyQueue, sellEntries } = fifoRecursive(buys, sell);
+
+    const expectedLeftoverBuys = new Queue([
+      mockTransaction(fixFloat(buy.amount - sell.amount), buy.time)
+    ]);
+    expect(buyQueue).toEqual(expectedLeftoverBuys);
+    expect(sellEntries).toEqual([
+      generateSellEntry(sell.amount, buy.time, sell.time)
     ]);
   });
 });
