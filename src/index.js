c50620a58d9179b4a679e02eee50461084d84e68
asarenski <asarenski@gmail.com>
2018-02-13 20:22:08 -0500

    got async queue working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 require('babel-polyfill');
 import _ from 'lodash';
 import axios from 'axios';
+import moment from 'moment';
+import async from 'async';
 import { parseAsync } from './csvParser';
 import validateOutputJson from './validateOutputJson';
 import parseHistoryJsonTypes from './parseHistoryJsonTypes';
@@ -8,6 +10,7 @@ import convertTypeToBuyOrSell from './convertTypeToBuyOrSell';
 import { convertedHistoryTypes } from './constants';
 import Queue from './Queue';
 import fifoRecursive from './fifoRecursive/fifoRecursive';
+
 
 _.mixin({
     parseHistoryJsonTypes,
@@ -68,10 +71,42 @@ const historyFilePath = '/home/asarenski/Downloads/history.csv';
     // https://docs.gdax.com/#api
     // https://api.gdax.com/products/btc-usd/candles?start=2018-02-10T01:00:00&end=2018-02-10T01:30:00&granularity=300
     // const FIVE_MINUTE_GRANULAR = 300;
-    // const [ gdaxTime, gdaxLow, gdaxHigh ] = gdaxResponse;
+    // const [ gdaxTime, gdaxLow, gdaxHigh, gdaxOpen, gdaxClose, gdaxVolume ] = gdaxResponse;
 
-    const url = 'https://api.gdax.com/products/btc-usd/candles?start=2018-02-10T01:00:00&end=2018-02-10T01:30:00&granularity=300';
-    const res = await axios.get(url);
-    const [ gdaxTime, gdaxLow, gdaxHigh ] = res.data[0];
+    const buildUrl = (start, end) => {
+        const FIVE_MINUTE_GRANULAR = 300;
+        return `https://api.gdax.com/products/btc-usd/candles?start=${start}&end=${end}&granularity=${FIVE_MINUTE_GRANULAR}`;
+    };
+
+    const urls = _.chain(sellEntries)
+        .reduce((acc, sellEntry) => {
+            const { amount, buyDate, saleDate } = sellEntry;
+            const formattedBuy = buyDate.format();
+            const formattedSale = saleDate.format();
+            if (!acc[formattedBuy]) {
+                acc[formattedBuy] = 0;
+            }
+            if (!acc[formattedSale]) {
+                acc[formattedSale] = 0;
+            }
+            return acc;
+        }, {})
+        .keys()
+        .map(date => moment(date))
+        .map(currentMoment => buildUrl(currentMoment.format(), currentMoment.add(5, 'minutes').format()))
+        .take(3) // REMOVE
+        .value();
+
+    async.mapLimit(urls, 1, (url, callback) => {
+        return axios.get(url)
+        .then(res => {
+            console.log('retrieving data...')
+            setTimeout(() => (callback(null, res.data)), 700);
+        })
+        .catch(e => { throw e });
+    }, (err, results) => {
+        if (err) throw err;
+        console.log(results);
+    });
 
 })();
