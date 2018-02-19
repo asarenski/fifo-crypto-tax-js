41018eca9ab756b1738390bea6ab677b436ad8bc
asarenski <asarenski@gmail.com>
2018-02-18 19:53:26 -0500

    added better logging

--- src/util/asyncGet.js
+++ src/util/asyncGet.js
@@ -2,11 +2,13 @@ import async from 'async';
 import axios from 'axios';
 
 const asyncGet = (urls, rateLimitTime) => {
+  console.log('getting pricing from gdax...');
+  let count = 1;
   return new Promise((resolve, reject) => {
       async.mapLimit(urls, 1, ({ key, url }, callback) => {
           return axios.get(url)
           .then(res => {
-              console.log('retrieving data...')
+              process.stdout.write(`position ${count++} of ${urls.length}\r`);
               setTimeout(() => (callback(null, { key, data: res.data })), rateLimitTime);
           })
           .catch(e => { throw e });
